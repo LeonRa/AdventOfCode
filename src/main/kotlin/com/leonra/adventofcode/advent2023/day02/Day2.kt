c4cfc089a6f5a301fd944b31e22906af0a512690
Leon Rabinovich <leon@duolingo.com>
2023-12-03 01:38:43 -0500

    Day 2

---
+++ src/main/kotlin/com/leonra/adventofcode/advent2023/day02/Day2.kt
@@ -0,0 +1,67 @@
+package com.leonra.adventofcode.advent2023.day02
+
+import com.leonra.adventofcode.shared.readResource
+import kotlin.math.max
+
+private val GAME_ID = Regex("Game (\\d+):")
+private val DELIMITER = Regex("[:;]")
+
+private val RED_BALLS = Regex("(\\d+) red")
+private val GREEN_BALLS = Regex("(\\d+) green")
+private val BLUE_BALLS = Regex("(\\d+) blue")
+
+private fun String.ballCount(ballRegex: Regex): Int =
+    ballRegex.find(this)?.let { it.groups[1]?.value?.toInt() } ?: 0
+
+private fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val redMax = 12
+    val greenMax = 13
+    val blueMax = 14
+
+    var sum = 0
+    readResource("/2023/day02/part1.txt") { line ->
+        var isGameValid = true
+        val pulls = line.split(DELIMITER)
+        for (pull in pulls) {
+            val redCount = pull.ballCount(RED_BALLS)
+            val greenCount = pull.ballCount(GREEN_BALLS)
+            val blueCount = pull.ballCount(BLUE_BALLS)
+
+            if (redCount > redMax || greenCount > greenMax || blueCount > blueMax) {
+                isGameValid = false
+                break
+            }
+        }
+        if (isGameValid) {
+            GAME_ID.find(line)?.let { sum += it.groups[1]?.value?.toInt() ?: 0 }
+        }
+    }
+    println("Part 1 sum: $sum")
+}
+
+private fun partTwo() {
+    var sum = 0
+    readResource("/2023/day02/part1.txt") { line ->
+        var minRed = 0
+        var minBlue = 0
+        var minGreen = 0
+
+        val pulls = line.split(DELIMITER)
+        for (pull in pulls) {
+            val redCount = pull.ballCount(RED_BALLS)
+            val greenCount = pull.ballCount(GREEN_BALLS)
+            val blueCount = pull.ballCount(BLUE_BALLS)
+
+            minRed = max(minRed, redCount)
+            minGreen = max(minGreen, greenCount)
+            minBlue = max(minBlue, blueCount)
+        }
+        sum += (minRed * minBlue * minGreen)
+    }
+    println("Part 2 sum: $sum")
+}
