f62caeb673ad01a5607d872a7d5732aac2328592
Leon Rabinovich <leon@duolingo.com>
2023-12-06 22:41:48 -0500

    Simplify resource reading helper

--- src/main/kotlin/com/leonra/adventofcode/shared/Resources.kt
+++ src/main/kotlin/com/leonra/adventofcode/shared/Resources.kt
@@ -7,8 +7,5 @@ import java.io.InputStreamReader
 fun readResource(path: String, doOnLine: (String) -> Unit) {
     val resource = requireNotNull(object {}.javaClass.getResource(path))
     val reader = BufferedReader(InputStreamReader(resource.openStream()))
-    var line: String?
-    while (reader.readLine().also { line = it } != null) {
-        doOnLine(requireNotNull(line))
-    }
+    reader.forEachLine(doOnLine)
 }
